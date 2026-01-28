6154739a783630d5a7f0237e28b0e126625fef1b
dawsonsk <dawsonsk@vcu.edu>
2026-01-28 01:35:56 -0500

    add script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong','arcade', 'pop'];
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button');
+    btn.classList.add('btn');
+    btn.innerText = sound;
+
+    btn.addEventListener('click', () => {
+        stopSounds();
+        document.getElementById(sound).play();
+    });
+        
+
+    document.getElementById('buttons').appendChild(btn);
+});
+
+function stopSounds() {
+    sounds.forEach((sound) => {
+        const snd = document.getElementById(sound);
+
+        snd.pause();
+        snd.currentTime = 0;
+    });
+}
+
